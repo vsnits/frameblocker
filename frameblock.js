38ee436773e999d5550835ce87f4f124695f72a0
Vsevolod Nitsenko <130823130+vsnits@users.noreply.github.com>
2023-04-18 09:06:05 +0300

    Update frameblock.js

--- frameblock.js
+++ frameblock.js
@@ -4,7 +4,9 @@
 
     let trusted = ["&google.com", "github.com", "lichess.org", "&youtube.com", "&wikipedia.org"]
     function includesDomain(list) {
-        /* Check whether the website is known app or is not.*/
+        /* 
+          Check whether the website is known web app or not.
+        */
         var doc = document.URL // shortcut the document.URL
         var host = "" // host like ‘http://’
         var res = "" // result like ‘****.com’
@@ -20,7 +22,7 @@
                 }
             var short =  "";
             while(short.length+1 < list[e].length) { short += list[e][short.length+1] }
-            var item = (list[e][0] == "&") ? (short) : list[e] // get correct name
+            var item = (list[e][0] == "&") ? (short) : list[e] // get correct name without &
             if(item == res) { return true }
             res = ""
             }; return false
@@ -28,36 +30,35 @@
 
    function insert(where, element, before) {
         /* 
-         Insert element before its following element.
-         Whether the following element `before` is not defined, then the element is last in its group, and so it just appends.
+          Function inserts element before parent's following element.
+          Whether the following element `before` is not defined, then the element is last in its group, and so it just appends.
         */        
         if(before) { where.insertBefore(element, before) }
         else { where.append(element) }
         };
 
-   var methods = new Array()   
-    /* 
-    An array of objects like { tag: "", filter: function(e) { return } } 
-    Elements list will be created by tag and every element of list goes through a filter as an argument. 
-    Whether `filter` returns true, element deleting from its parent. 
-   */
+   var methods = new Array()
     methods.push({ tag: 'div', filter: function(e) {
             /* 
-             Checking for shadow DOM,
-             There are no methods to remove shadowRoot, however we can remove shadowRoot's parent element. 
-             The idea is to remove element and carefully replace with its clone, therefore delete shadowRoot.
+              Checking for shadow DOM.
+              There are no methods to remove shadowRoot, however we can remove shadowRoot's parent element. 
+              The idea is to remove element and carefully replace with its clone, therefore delete shadowRoot.
             */
             if(!e.children.length && innerWidth*innerHeight*0.02 < e.offsetWidth*e.offsetHeight) {
-                /* Only large empty divs are checked */
+                /*
+                  Only large empty divs are checked as it's faster.
+                */
                 var hand = e.parentElement
                 var group = hand.children 
-                var place = 0 // define position where div is in group
+                var place = 0
                 while(place < group.length) {
-                    if(group[place] == e) { break } // position now defined
+                    if(group[place] == e) { break }
                     else { place++ }
-                    } // the position will be defined in all cases, as we know that group contains all children `hand`
+                    } // the position will be defined in all cases, as we know that group contains all children of `hand`
                 hand.removeChild(e) // now remove original element and replace with its clone
-                /* Element also removed from its group, so (place+1) become just (place) */
+                /* 
+                   Element also removed from its group, so (place+1) become just (place)
+                 */
                 insert(hand, e.cloneNode(29), group[place]) // inserting to original position
                 return false // no need to remove element
                 }
@@ -68,32 +69,38 @@
     methods.push({ tag: 'yatag', filter: function(e) { return true } }) // Removing a kind of banners
     methods.push({ tag: 'a', filter: function(e) { // And remove banners itself
             if(e.getElementsByTagName("img").length || e.getElementsByTagName("video").length) {
-                /* Whether the link has a child image or video, it is a banner. */
-                return true // removing
+                /* 
+                   Whether the link has a child image or video, it is like a banner.
+                */
+                return true
                 }
             } 
         })
     methods.push({ tag: 'video', filter: function(e) {
-            if(e.autoplay || e.playsinline || e.muted) { return true } // try remove autoplay
+            if(e.autoplay || e.playsinline || e.muted) { return true } // try to remove autoplaying video
             } 
         })
-    function block(includes_domain=null) { // reading methods
-        if(includes_domain==false || block.target) { // whether domain not found
-            block.target = true // indicate that domain not found
-            var blocked = new Array() // create an array of removed elements
+    function block(includes_domain=null) {
+        if(includes_domain==false || block.target) {
+            block.target = true
+            var blocked = new Array() 
             function remove(tag, filter=function() { return true }) { // remove elements by tag
-                /* Function takes not only tag names but also an arrays */
+                /*
+                  Function takes not only tag names but also an arrays
+                */
                 var things; (typeof tag == 'string') ? things = document.getElementsByTagName(tag) : things = tag
                 for(var i = 0; i < things.length; i++) { // filtering items
-                    /* Whether filter is true, remember element and remove */
-                    if(filter(things[i])) { blocked.push(things[i]); things[i].parentElement.removeChild(things[i]); }
+                    /* 
+                       Whether filter is true, remember element and remove
+                    */
+                    if(filter(things[i])) { blocked.push(things[i]); things[i].parentElement.removeChild(things[i]) }
                     }
                 }
             for(var i = 0; i < methods.length; i++) { // finally go through the all methods
-                remove(methods[i].tag, methods[i].filter) // removing
+                remove(methods[i].tag, methods[i].filter) 
                 }; if(blocked.length) { console.log("frames blocked > ", blocked) } // and.. output the removed items
-             } else if(includes_domain == true) { console.log("Good website!") }
-           setTimeout(function() { if(block.target == true) { block() } }, 900) // repeat every 900 ms
+             } else if(includes_domain == true) { console.log("Good website!") } // in this case repetition will stop
+           setTimeout(function() { if(block.target == true) { block() } }, 900) // repeat action every 900 ms
         }; block( includesDomain(trusted) ) // finally call the function
 
     /**/
